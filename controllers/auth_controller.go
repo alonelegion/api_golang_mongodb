7dee88c064431fb34d2b91e475a803d08eb9195d
alonelegion <com.berdin@gmail.com>
2022-12-29 16:26:32 +0400

    add verify email

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"html/template"
 	"log"
 	"net/http"
@@ -44,6 +45,7 @@ func (ac *AuthController) SignUpUser(ctx *gin.Context) {
 
 	if user.Password != user.PasswordConfirm {
 		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Passwords do not match"})
+		return
 	}
 
 	newUser, err := ac.authService.SignUpUser(user)
@@ -61,7 +63,7 @@ func (ac *AuthController) SignUpUser(ctx *gin.Context) {
 		log.Fatal("Could not load config", err)
 	}
 
-	// Generate verification code
+	// Generate Verification Code
 	code := randstr.String(20)
 
 	verificationCode := utils.Encode(code)
@@ -74,12 +76,13 @@ func (ac *AuthController) SignUpUser(ctx *gin.Context) {
 		firstName = strings.Split(firstName, " ")[1]
 	}
 
-	// Send Email
+	// ? Send Email
 	emailData := utils.EmailData{
-		URL:       config.Origin + "/verifiemail/" + code,
+		URL:       config.Origin + "/verifyemail/" + code,
 		FirstName: firstName,
-		Subject:   "Your account verification cade",
+		Subject:   "Your account verification code",
 	}
+
 	err = utils.SendEmail(newUser, &emailData, ac.temp, "verificationCode.html")
 	if err != nil {
 		ctx.JSON(http.StatusBadGateway, gin.H{"status": "success", "message": "There was an error sending email"})
@@ -173,4 +176,27 @@ func (ac *AuthController) LogoutUser(ctx *gin.Context) {
 	ctx.SetCookie("logged_in", "", -1, "/", "localhost", false, true)
 
 	ctx.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+func (ac *AuthController) VerifyEmail(ctx *gin.Context) {
+	code := ctx.Params.ByName("verificationCode")
+	verificationCode := utils.Encode(code)
+
+	query := bson.D{{Key: "verificationCode", Value: verificationCode}}
+	update := bson.D{{Key: "$set", Value: bson.D{
+		{Key: "verified", Value: true}}},
+		{Key: "$unset", Value: bson.D{{Key: "verificationCode", Value: ""}}}}
+	result, err := ac.collection.UpdateOne(ac.ctx, query, update)
+	if err != nil {
+		ctx.JSON(http.StatusForbidden, gin.H{"status": "success", "message": err.Error()})
+		return
+	}
+	if result.MatchedCount == 0 {
+		ctx.JSON(http.StatusForbidden, gin.H{"status": "success", "message": "Could not verify email address"})
+		return
+	}
+
+	fmt.Println(result)
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": "Email verified successfully"})
 }
